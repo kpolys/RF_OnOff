95f63be167a3756a1dfcd174936a991bb598462b
webs1821 <github@k13.pl>
2015-03-24 22:19:09 +0100

    Initial commit

---
+++ RF_OnOff.ino
@@ -0,0 +1,54 @@
+
+#include <RCSwitch.h>
+#define ON 5891 // on 5314307
+#define OFF 5900 // off 5314316
+#define INR 0 // pin 2
+#define LED 13
+#define BUZ 10
+
+#define DEBUG
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() 
+{
+	mySwitch.enableReceive(INR);  
+	#ifdef DEBUG
+		Serial.begin(9600);
+		Serial.println("Start");
+		digitalWrite(BUZ, HIGH);
+		delay(500);
+		digitalWrite(BUZ, LOW);
+	#endif
+}
+
+void loop() 
+{
+	if (mySwitch.available()) 
+	{
+		int value = mySwitch.getReceivedValue();
+		if (value != 0) 
+		{
+			if (value == ON)
+			{
+				digitalWrite(BUZ, HIGH);
+				digitalWrite(LED, HIGH);
+				#ifdef DEBUG
+					Serial.print("HIGH! ");
+				#endif
+			}
+
+			if (value == OFF)
+			{
+				digitalWrite(BUZ, LOW);
+				digitalWrite(LED, LOW);
+				#ifdef DEBUG
+					Serial.print("low ");
+				#endif
+			}	
+		}
+		mySwitch.resetAvailable();
+	}
+}
+
+
